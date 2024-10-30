32a5581db879c0f2782779998a5a25de444f748f
Andrei Vintila <vintila.andrei@gmail.com>
2024-10-30 14:37:08 +0200

    added alias for auth utils

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,14 @@
+import { createResolver } from "@nuxt/kit";
+const { resolve } = createResolver(import.meta.url);
+
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: [
     'nuxt-auth-utils',
     '@nuxthub/core'
   ],
+  alias: { "#auth-layer": resolve("./") },
   hub: {
     database: true
   },
